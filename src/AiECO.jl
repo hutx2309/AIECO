115a5f2b2f93a483cd4117dc310046a603be5d6e
Tony Hu <C838275691@colostate.edu>
2026-06-17 12:26:22 -0600

    Add Julia package scaffold

---
+++ src/AiECO.jl
@@ -0,0 +1,8 @@
+module AiECO
+
+export greet
+
+"""Return a simple greeting from the package."""
+greet() = "Hello from AiECO"
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Test
+using AiECO
+
+@testset "AiECO" begin
+    @test greet() == "Hello from AiECO"
+end
